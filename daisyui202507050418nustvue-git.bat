ceae880cf36ff244f48761b60e6d6c10bb8f8afc
goldshoot0720 <goldshoot0720@gmail.com>
2025-06-26 21:29:45 +0800

    netlify gitlab youtube upto 75 bilibli upto 75 20250705 0418

--- daisyui202507050418nustvue-git.bat
+++ daisyui202507050418nustvue-git.bat
@@ -2,7 +2,7 @@ git init
 
 git add .
  
-git commit -m "202507050418 netlify gitlab youtube upto 75 bilibli upto 75"
+git commit -m "netlify gitlab youtube upto 75 bilibli upto 75 20250705 0418"
 
 git remote add origin https://github.com/abuhg17/daisyui202507050418nustvue.git
 
